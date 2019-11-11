d99d54dede511599db47004adddf60de4f0c18e6
Roman Silivestrov <rsilivestr@gmail.com>
2019-11-11 20:22:34 +0300

    added scss & pug

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import './index.scss';
 
 function component() {
   const element = document.createElement('div');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,36 @@
-const path = require('path');
+const path = require('path'),
+  HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.pug'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+            'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.pug$/,
+        use: [
+          'pug-loader',
+        ],
+      },
+    ],
   },
 };
