4b0518ac805cda1345974ce584b9af5f4e629cd6
Roman Silivestrov <rsilivestr@gmail.com>
2019-11-13 21:37:28 +0300

    added image copying, footer component, stripped config of comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import './index.sass';
-import './static/favicon.png';
-import './static/bf.jpg';
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
 
-// let c = ((a,b) => a + b)(1, 1);
 console.log(((a,b) => a + b)(1, 9));
--- webpack.common.js
+++ webpack.common.js
@@ -2,34 +2,28 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
+    app: './src',
   },
   output: {
-    // filename: '[name].[contenthash].js',
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
-    // A webpack plugin to remove/clean your build folder(s).
     new CleanWebpackPlugin(),
-    // Plugin that simplifies creation of HTML files to serve your bundles
     new HtmlWebpackPlugin({
       template: './src/index.pug'
     }),
     new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // all options are optional
       filename: '[name].css',
-      // chunkFilename: '[id].css',
-      ignoreOrder: false, // Enable to remove warnings about conflicting order
+      ignoreOrder: false,
     }),
-    // new CopyWebpackPlugin([
-    //   {from:'src/static',to:'static'} 
-    // ]),
+    new CopyWebpackPlugin([
+      {from:'src/images',to:'images'} 
+    ]),
   ],
   module: {
     rules: [
@@ -47,30 +41,22 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         include: path.resolve(__dirname, 'src'),
         use: [
-          // // Creates `style` nodes from JS strings
-          //   'style-loader',
           {
             loader: MiniCssExtractPlugin.loader,
-            // options: {
-            //   hmr: process.env.NODE_ENV === 'development',
-            // },
           },
-          // Translates CSS into CommonJS
           {
             loader: 'css-loader',
             options: { importLoaders: 1 }
           },
-          // place after css-loader and before sass-loader
           {
             loader: 'postcss-loader',
             options: {
-              ident: 'postcss',        
+              ident: 'postcss',
               plugins: [
                 require('autoprefixer'),
               ]
             }
           },
-          // Compiles Sass to CSS
           'sass-loader',
         ],
       },
@@ -90,8 +76,9 @@ module.exports = {
       },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
+        include: path.resolve(__dirname, 'src/images'),
         use: [
-          'file-loader?name=static/[name].[ext]',
+          'file-loader?name=images/[name].[ext]',
           {
             loader: 'image-webpack-loader',
             options: {
@@ -99,7 +86,6 @@ module.exports = {
                 progressive: true,
                 quality: 65
               },
-              // optipng.enabled: false will disable optipng
               optipng: {
                 enabled: false,
               },
@@ -115,8 +101,5 @@ module.exports = {
         ],
       },
     ],
-  },
-  resolve: {
-    extensions: ['.gif', '.png', '.jpg', '.jpeg', '.svg']
   },
 };
