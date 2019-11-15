d980c167323cbb44b061e7ca4273ac7f4a44862e
rsilivestr <rsilivestr@gmail.com>
2019-11-15 16:26:35 +0300

    solved fonts problem with url-loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'Assets/fonts/mont/face.css'
+
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
--- webpack.common.js
+++ webpack.common.js
@@ -74,6 +74,12 @@ module.exports = {
           'style-loader', 'css-loader'
         ],
       },
+      { 
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+        use: [
+          'url-loader?limit=100000'
+        ]
+      },
       {
         test: /\.pug$/,
         include: path.resolve(__dirname, 'src'),
