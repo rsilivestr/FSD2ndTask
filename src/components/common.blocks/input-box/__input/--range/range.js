adb919ef0be1c46fcf95f07c303c5437c96e07de
Roman Silivestrov <rsilivestr@gmail.com>
2019-11-28 18:59:58 +0300

    added button block

--- src/components/common.blocks/input-box/__input/--range/range.js
+++ src/components/common.blocks/input-box/__input/--range/range.js
@@ -3,18 +3,20 @@ import noUiSlider from 'nouislider';
 const UIrange = document.querySelector('.input-box__range');
 const UIrangeLabel = document.querySelector('.input-box__range-label');
 
-noUiSlider.create(UIrange, {
-  start: [5000, 10000],
-  step: 500,
-  behaviour: 'drag',
-  connect: true,
-  range: {
-    'min': 0,
-    'max': 15000
-  }
-});
-
-UIrange.noUiSlider.on('update', () => {
-  const rangeValues = UIrange.noUiSlider.get().map(x => parseInt(x));
-  UIrangeLabel.innerText = `${rangeValues[0]}₽ - ${rangeValues[1]}₽`;
-});
+if(UIrange) {
+  noUiSlider.create(UIrange, {
+    start: [5000, 10000],
+    step: 500,
+    behaviour: 'drag',
+    connect: true,
+    range: {
+      'min': 0,
+      'max': 15000
+    }
+  });
+  
+  UIrange.noUiSlider.on('update', () => {
+    const rangeValues = UIrange.noUiSlider.get().map(x => parseInt(x));
+    UIrangeLabel.innerText = `${rangeValues[0]}₽ - ${rangeValues[1]}₽`;
+  });
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'CommonBlocks/input-box/__input/__input.js';
 
 const routerNav = document.querySelector('.router__nav'),
       routerIframe = document.querySelector('.router__current-page');
+
 if(routerNav) {
   routerNav.addEventListener('click', e => {
     if(e.target.classList.contains('router__navlink')) {
@@ -11,40 +12,3 @@ if(routerNav) {
     }
   });
 }
-
-// const UIdateMaskFields = document.querySelectorAll('.input-box__input--masked-date');
-
-// UIdateMaskFields.forEach(dateMask => {
-//   IMask(
-//     dateMask,
-//     {
-//       mask: Date,
-//       overwrite: true,
-//       autofix: true,
-//       blocks: {
-//         d: {
-//           mask: IMask.MaskedRange, 
-//           placeholderChar: 'Д', 
-//           from: 1, 
-//           to: 31, 
-//           maxLength: 2
-//         },
-//         m: {
-//           mask: IMask.MaskedRange, 
-//           placeholderChar: 'М', 
-//           from: 1, 
-//           to: 12, 
-//           maxLength: 2
-//         },
-//         Y: {
-//           mask: IMask.MaskedRange, 
-//           placeholderChar: 'Г', 
-//           from: 1900, 
-//           to: 2999, 
-//           maxLength: 4
-//         }
-//       },
-//       lazy: false
-//     }
-//   );
-// });
