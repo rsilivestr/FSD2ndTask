c7a86ada2d2e8833714bfbd41931c6302e724e9a
Roman Silivestrov <rsilivestr@gmail.com>
2019-12-03 08:40:07 +0300

    continue dropdown refactoring

--- src/components/common.blocks/dropdown/dropdown.js
+++ src/components/common.blocks/dropdown/dropdown.js
@@ -15,7 +15,7 @@ const wrapClass = 'dropdown',
       btnClass = 'button',
       applyBtnClass = 'dropdown__btn--apply',
       clearBtnClass = 'dropdown__btn--clear',
-      UIdrops = document.querySelectorAll('.'+wrapClass);
+      UIdrops = document.querySelectorAll('.' + wrapClass);
 
 document.body.addEventListener('mousedown', toggleDropdown);
 
@@ -28,20 +28,20 @@ function toggleDropdown(e) {
     UIdrops.forEach(drop => {
       if(drop !== UIcurrent) {
         drop.classList.remove(openClass);
-        drop.querySelector('.'+contentsClass).classList.remove(contentsVisibleClass);
+        drop.querySelector('.' + contentsClass).classList.remove(contentsVisibleClass);
       }
     });
     // toggle clicked dropdown
     UIcurrent.classList.toggle(openClass);
     UIconts.classList.toggle(contentsVisibleClass);
   // when dropdown-content is clicked
-  } else if (e.target.closest('.'+contentsClass)) {
+  } else if (e.target.closest('.' + contentsClass)) {
 
 
     // when control (add / subtract) is clicked
     if(e.target.classList.contains(controlClass)) {
       updateOption(e.target);
-      if (e.target.closest('.'+discreteOutClass)) {
+      if (e.target.closest('.' + discreteOutClass)) {
     
 
         
@@ -60,7 +60,7 @@ function toggleDropdown(e) {
 
 
 
-      } else if (e.target.closest('.'+singleOutClass)) {
+      } else if (e.target.closest('.' + singleOutClass)) {
     
         
         // count guests (with apply, clear buttons)
@@ -70,7 +70,7 @@ function toggleDropdown(e) {
       }
 
 
-    } else if (e.target.classList.contains('.'+btnClass)) {
+    } else if (e.target.classList.contains('.' + btnClass)) {
       console.log('drop btn');
 
 
@@ -79,7 +79,7 @@ function toggleDropdown(e) {
     // close any and all dropdowns if none targeted
     UIdrops.forEach(drop => {
       drop.classList.remove(openClass);
-      drop.querySelector('.'+contentsClass).classList.remove(contentsVisibleClass);
+      drop.querySelector('.' + contentsClass).classList.remove(contentsVisibleClass);
     });
   }
 }
@@ -90,21 +90,22 @@ function updateOption(btn) {
         minValue = opt.dataset.min ? opt.dataset.min: 0,
         maxValue = opt.dataset.max ? opt.dataset.max : 99,
         action = btn.classList.contains(addBtnClass) ? 'add' : 'subtract',
-        UIvalue = btn.nextElementSibling ? btn.nextElementSibling : btn.previousElementSibling;
+        // UIvalue = btn.nextElementSibling ? btn.nextElementSibling : btn.previousElementSibling;
+        UIvalue = opt.querySelector('.' + optValClass);
   let currentValue = UIvalue.innerText,
 
 
-        isDisplaySingle = btn.closest('.'+wrapClass).classList.contains(singleOutClass);
+        isDisplaySingle = btn.closest('.' + wrapClass).classList.contains(singleOutClass);
 
 
   if (action == 'subtract' && parseInt(currentValue) > parseInt(minValue)) {
     currentValue--;
     UIvalue.innerText = currentValue;
-    updateValue(opt, minValue, maxValue, currentValue)
+    updateValues(opt)
   } else if (action == 'add' && parseInt(currentValue) < parseInt(maxValue)) {
     currentValue++
     UIvalue.innerText = currentValue;
-    updateValue(opt, minValue, maxValue, currentValue)
+    updateValues(opt)
   }
   if (!isDisplaySingle) {
     // console.log('discrete');
@@ -115,13 +116,47 @@ function updateOption(btn) {
 }
 
 // enable & disable controls on min / max value
-function updateValue(opt, min, max, cur) {
-  const UIsubtract = opt.querySelector('.'+subtractBtnClass),
-        UIadd = opt.querySelector('.'+addBtnClass)
-  if(cur == min) {
-    UIsubtract.classList.add(disabledBtnClass);
+// function updateValue(opt, min, max, cur) {
+//   const UIsubtract = opt.querySelector('.' + subtractBtnClass),
+//         UIadd = opt.querySelector('.' + addBtnClass)
+//   if(cur == min) {
+//     UIsubtract.classList.add(disabledBtnClass);
+//   } else {
+//     UIsubtract.classList.remove(disabledBtnClass);
+//   }
+//   if(cur == max) {
+//     UIadd.classList.add(disabledBtnClass);
+//   } else {
+//     UIadd.classList.remove(disabledBtnClass);
+//   }
+// }
+
+function updateValues(opt = null) {
+  if (opt) {
+    // update single option
+    updateSingleOption(opt);
   } else {
-    UIsubtract.classList.remove(disabledBtnClass);
+    // update values in all dropdowns
+    const UIdrops = document.querySelectorAll('.' + discreteOutClass);
+    UIdrops.forEach(drop => {
+      const UIopts = drop.querySelectorAll('.' + optionClass);
+      UIopts.foreach(opt => updateSingleOption(opt));
+    });
+  }
+}
+
+function updateSingleOption(opt) {
+  // check dropdown type here
+  const UIsub = opt.querySelector('.' + subtractBtnClass),
+        UIadd = opt.querySelector('.' + addBtnClass),
+        UIvalue = opt.querySelector('.' + optValClass),
+        cur = parseInt(UIvalue.innerText),
+        min = opt.dataset.min ? parseInt(opt.dataset.min) : 0,
+        max = opt.dataset.max ? parseInt(opt.dataset.max) : 99;
+  if(cur == min) {
+    UIsub.classList.add(disabledBtnClass);
+  } else {
+    UIsub.classList.remove(disabledBtnClass);
   }
   if(cur == max) {
     UIadd.classList.add(disabledBtnClass);
@@ -129,8 +164,6 @@ function updateValue(opt, min, max, cur) {
     UIadd.classList.remove(disabledBtnClass);
   }
 }
-
-
 
 
 
@@ -201,63 +234,63 @@ function discreteUpdateDisplay(el) {
 
 
 
-// handle subtraction / addition of values
-function changeDropdown(el) {
-  // define drop contents
-  const UIconts = el.closest('.'+contentsClass);
-  // define main OUTPUT (.input-box__input--dropdown)
-  const UIdisplay = UIconts.previousElementSibling;
-  // defune array of option elements
-  const opts = [];
-  // populate array
-  UIconts.querySelectorAll('.'+optionClass).forEach(opt => {
-    opts.push(opt);
-  });
-  // define array of option values
-  const optionValues = [];
-  // populate array with option values
-  // separate function for array and sum?
-  const optionValueElements = UIconts.querySelectorAll('.'+optValClass);
-  // handle addition
-  if(el.classList.contains(addBtnClass)) {
-    const currentOption = el.closest('.'+optionClass);
-    // which option was targeted??? splice to optionValues array
-    opts.forEach(opt => {
-      if(opt == currentOption) {
-        const currentVal = el.previousElementSibling;
-        currentVal.innerText = parseInt(currentVal.innerText) + 1;
-        updateValues(optionValueElements, optionValues, UIdisplay);
-      }
-    });
-  } else if(el.classList.contains(subtractBtnClass)) {
-    // const currentOption = el.closest('.input-box__dropdown-option');
-    // which option was targeted??? splice to optionValues array
-    // opts.forEach(opt => {
-    //   if(opt == currentOption) {
-      const currentVal = el.nextElementSibling;
-      // subtract only if greater than 0
-      if(parseInt(currentVal.innerText) > 0) {
-        currentVal.innerText = parseInt(currentVal.innerText) - 1;
-        updateValues(optionValueElements, optionValues, UIdisplay);
-      }
-    //   }
-    // });
-  }
-}
+// // handle subtraction / addition of values
+// function changeDropdown(el) {
+//   // define drop contents
+//   const UIconts = el.closest('.' + contentsClass);
+//   // define main OUTPUT (.input-box__input--dropdown)
+//   const UIdisplay = UIconts.previousElementSibling;
+//   // defune array of option elements
+//   const opts = [];
+//   // populate array
+//   UIconts.querySelectorAll('.' + optionClass).forEach(opt => {
+//     opts.push(opt);
+//   });
+//   // define array of option values
+//   const optionValues = [];
+//   // populate array with option values
+//   // separate function for array and sum?
+//   const optionValueElements = UIconts.querySelectorAll('.' + optValClass);
+//   // handle addition
+//   if(el.classList.contains(addBtnClass)) {
+//     const currentOption = el.closest('.' + optionClass);
+//     // which option was targeted??? splice to optionValues array
+//     opts.forEach(opt => {
+//       if(opt == currentOption) {
+//         const currentVal = el.previousElementSibling;
+//         currentVal.innerText = parseInt(currentVal.innerText) + 1;
+//         updateValues(optionValueElements, optionValues, UIdisplay);
+//       }
+//     });
+//   } else if(el.classList.contains(subtractBtnClass)) {
+//     // const currentOption = el.closest('.input-box__dropdown-option');
+//     // which option was targeted??? splice to optionValues array
+//     // opts.forEach(opt => {
+//     //   if(opt == currentOption) {
+//       const currentVal = el.nextElementSibling;
+//       // subtract only if greater than 0
+//       if(parseInt(currentVal.innerText) > 0) {
+//         currentVal.innerText = parseInt(currentVal.innerText) - 1;
+//         updateValues(optionValueElements, optionValues, UIdisplay);
+//       }
+//     //   }
+//     // });
+//   }
+// }
 
-function updateValues(elArr, valArr, sum) {
-  elArr.forEach(elem => {
-    // update options values
-    valArr.push(parseInt(elem.innerText));
-    // display sum of options array
-    sum.value = valArr.reduce((a,b) => a + b);
-    // change placeholder to default if sum is 0
-    if(sum.value == 0) {
-      // sum.value = sum.dataset.placeholder;
-      sum.value = '';
-    }
-  });
-}
+// function updateValues(elArr, valArr, sum) {
+//   elArr.forEach(elem => {
+//     // update options values
+//     valArr.push(parseInt(elem.innerText));
+//     // display sum of options array
+//     sum.value = valArr.reduce((a,b) => a + b);
+//     // change placeholder to default if sum is 0
+//     if(sum.value == 0) {
+//       // sum.value = sum.dataset.placeholder;
+//       sum.value = '';
+//     }
+//   });
+// }
 
 // check if sum is not greater then room capacity
 
