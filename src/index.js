0ee6a4caf7bbf41128957f08a5b57dcfbf28a001
Roman Silivestrov <rsilivestr@gmail.com>
2019-12-06 19:23:29 +0300

    Page Elements somewhat done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import 'CommonBlocks/input-box/__input/__input.js';
 import 'CommonBlocks/dropdown/dropdown.js';
 
-const routerNav = document.querySelector('.router__nav'),
-      routerIframe = document.querySelector('.router__current-page');
+// const routerNav = document.querySelector('.router__nav'),
+//       routerIframe = document.querySelector('.router__current-page');
 
-if(routerNav) {
-  routerNav.addEventListener('click', e => {
-    if(e.target.classList.contains('router__navlink')) {
-      e.preventDefault();
+// if(routerNav) {
+//   routerNav.addEventListener('click', e => {
+//     if(e.target.classList.contains('router__navlink')) {
+//       e.preventDefault();
 
-      routerIframe.src = e.target.href;
-    }
-  });
-}
+//       routerIframe.src = e.target.href;
+//     }
+//   });
+// }
