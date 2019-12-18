cf5b6db97523146f61e800fd423f6417c6793593
Roman Silivestrov <rsilivestr@gmail.com>
2019-12-18 21:03:52 +0300

    added double date dropdown scripts

--- src/components/common.blocks/date-dropdown/date-dropdown.js
+++ src/components/common.blocks/date-dropdown/date-dropdown.js
@@ -70,9 +70,6 @@ if (UIdoublePickers) {
     const UIdatepickerStart = picker.querySelector('.date-dropdown__start'),
       UIdatepickerEnd = picker.querySelector('.date-dropdown__end');
 
-      console.log(UIdatepickerStart)
-      console.log(UIdatepickerEnd)
-
     $(picker).datepicker({
       prevHtml: '<i class="material-icons datepicker__arrow">arrow_back</i>',
       nextHtml: '<i class="material-icons datepicker__arrow">arrow_forward</i>',
@@ -86,27 +83,59 @@ if (UIdoublePickers) {
       classes: 'form__datepicker'
     });
 
-    // const UIdatepickerCalendar = picker.querySelector('.datepicker-inline');
+    // get created datepicker block
     const UIdatepickerCalendar = picker.querySelector('.form__datepicker');
+
+    // get datapicker object to call functions
+    const pickerObject = $(picker).datepicker().data('datepicker');
+
+    // append buttons
+    const buttonsPanel = document.createElement('div');
+    buttonsPanel.className = 'datepicker__buttons-panel';
+    UIdatepickerCalendar.appendChild(buttonsPanel);
+
+    const clearBtn = document.createElement('button');
+    clearBtn.className = 'button button--style_text button--size_auto datepicker__clear-btn';
+    clearBtn.innerText = 'очистить';
+    buttonsPanel.appendChild(clearBtn);
+
+    const applyBtn = document.createElement('button');
+    applyBtn.className = 'button button--style_text button--size_auto datepicker__apply-btn';
+    applyBtn.innerText = 'применить';
+    buttonsPanel.appendChild(applyBtn);
 
     document.body.addEventListener('click', e => {
       // if UIdatepickerStart or UIdatepickerEnd is targeted
       if (e.target.classList.contains('date-dropdown__display')) {
         UIdatepickerCalendar.classList.add('form__datepicker--visible');
+      } else if (e.target.classList.contains('datepicker__clear-btn')) {
+        // clear calendar
+        pickerObject.clear();
+        // clear input fields
+        UIdatepickerStart.value = '';
+        UIdatepickerEnd.value = '';
+      } else if (e.target.classList.contains('datepicker__apply-btn')) {
+        // get selected dates
+        const dates = pickerObject.selectedDates;
+        // formant and append dates
+        const startDateString = dates[0].toLocaleDateString('ru-RU');
+        UIdatepickerStart.value = startDateString;
+        const endDateString = dates[1].toLocaleDateString('ru-RU');
+        UIdatepickerEnd.value = endDateString;
+        // close calendar
+        UIdatepickerCalendar.classList.remove('form__datepicker--visible');
       } else if (
         // could not capture all insides with closest so here we go
-        e.target.closest('.datepicker--nav') || 
-        e.target.classList.contains('datepicker--nav-title') || 
-        e.target.classList.contains('datepicker--nav-action') || 
-        e.target.classList.contains('datepicker__arrow') || 
-        e.target.classList.contains('datepicker--cells') || 
-        e.target.classList.contains('datepicker--cell') || 
-        e.target.classList.contains('datepicker--day-name') ||
-        e.target.classList.contains('datepicker--content')
+        !e.target.classList.contains('datepicker--nav') &&
+        !e.target.classList.contains('datepicker--nav-title') &&
+        !e.target.classList.contains('datepicker--nav-action') &&
+        !e.target.classList.contains('datepicker__arrow') &&
+        !e.target.classList.contains('datepicker--cells') &&
+        !e.target.classList.contains('datepicker--cell') &&
+        !e.target.classList.contains('datepicker--day-name') &&
+        !e.target.classList.contains('datepicker--content') &&
+        !e.target.classList.contains('datepicker__buttons-panel')
       ) {
-        console.log(123)
-      } else {
-        console.log(e.target)
         UIdatepickerCalendar.classList.remove('form__datepicker--visible');
       }
     });
