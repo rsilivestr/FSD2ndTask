47c488f9ffa3c4916c5d4c864dfa3a406e5b5fbb
Roman Silivestrov <rsilivestr@google.com>
2019-12-31 15:20:33 +0300

    changed assets for github pages

--- webpack.common.js
+++ webpack.common.js
@@ -118,7 +118,7 @@ module.exports = {
             loader: MiniCssExtractPlugin.loader,
             options: {
               // Specifies a custom public path for the target file(s).
-              publicPath: '/',
+              publicPath: '/FSD2ndTask/dist/',
             },
           },
           'css-loader'
