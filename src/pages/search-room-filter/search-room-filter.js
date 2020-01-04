d4ee3a83bf9adfee4c9e1f485578cb2b6684cc5b
Roman Silivestrov <rsilivestr@gmail.com>
2020-01-04 15:34:43 +0300

    bugfixes
    added main-content class (dynamic padding) to search-room page wrapper,
    added scroll to filters on mobile
    pagination's divs to links

--- src/pages/search-room-filter/search-room-filter.js
+++ src/pages/search-room-filter/search-room-filter.js
@@ -1,13 +1,15 @@
-const UIexpandBtn = document.querySelector('.search-filter-page__expand-filters');
-const UIfiltersPanel = document.querySelector('.search-filter-page__filters');
+const UIfiltersPage = document.querySelector('.search-room-page'),
+  UIexpandBtn = document.querySelector('.search-room-page__expand-filters'),
+  UIfiltersPanel = document.querySelector('.search-room-page__filters');
 
 if (UIexpandBtn) {
   UIexpandBtn.addEventListener('click', toggleFliters);
 }
 
 function toggleFliters () {
-  UIfiltersPanel.classList.toggle('search-filter-page__filters--expanded');
-  UIexpandBtn.classList.toggle('search-filter-page__expand-filters--expanded');
+  UIfiltersPage.classList.toggle('search-room-page--noscroll');
+  UIfiltersPanel.classList.toggle('search-room-page__filters--expanded');
+  UIexpandBtn.classList.toggle('search-room-page__expand-filters--expanded');
 }
 
 // remove class on resize
