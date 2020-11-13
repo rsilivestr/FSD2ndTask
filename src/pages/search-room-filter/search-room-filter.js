4a6420b4a3e5c00abd77c8b415f242872ed5d8f6
Roman Silivestrov <rsilivestr@gmail.com>
2020-11-13 18:41:31 +0300

    Remove from search-room-filter js for filters panel

--- src/pages/search-room-filter/search-room-filter.js
+++ src/pages/search-room-filter/search-room-filter.js
@@ -6,19 +6,3 @@ if (document.querySelector('.page--layout_search-filter')) {
 
   new FSD2ndTaskRange('.js-rangeslider');
 }
-
-const UIfiltersPage = document.querySelector('.search-room-page'),
-  UIexpandBtn = document.querySelector('.search-room-page__expand-filters'),
-  UIfiltersPanel = document.querySelector('.search-room-page__filters');
-
-if (UIexpandBtn) {
-  UIexpandBtn.addEventListener('click', toggleFliters);
-}
-
-function toggleFliters() {
-  UIfiltersPage.classList.toggle('search-room-page--noscroll');
-  UIfiltersPanel.classList.toggle('search-room-page__filters--expanded');
-  UIexpandBtn.classList.toggle('search-room-page__expand-filters--expanded');
-}
-
-// remove class on resize
