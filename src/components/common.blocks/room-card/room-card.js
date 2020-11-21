d2639f358b6c1da46096ef879ab01213db371a7a
Roman Silivestrov <rsilivestr@gmail.com>
2020-11-21 14:26:18 +0300

    Update room-card mixin, js, styles

--- src/components/common.blocks/room-card/room-card.js
+++ src/components/common.blocks/room-card/room-card.js
@@ -5,12 +5,12 @@ const sliders = Array.from(document.querySelectorAll('.room-card__photos'));
 const sliderSelectors = {
   slider: '.room-card__photos',
   slide: '.room-card__photo',
-  nav: '.room-card__dots',
-  navButton: '.room-card__dot',
+  nav: '.room-card__nav',
+  navButton: '.room-card__navbtn',
   control: '.room-card__control',
   controlLeft: '.room-card__control--direction_left',
   controlRight: '.room-card__control--direction_right',
-  currentNavButtonClass: 'room-card__dot--current',
+  currentNavButtonClass: 'room-card__navbtn--current',
 };
 
 sliders.forEach((slider) => new FSD2ndTaskRoomSlider(slider, sliderSelectors));
