660b1d8cf5642658400098b108c212c3a3420821
Roman Silivestrov <rsilivestr@gmail.com>
2020-11-21 16:41:13 +0300

    Update room-details: diagram stroke width, fix .main-content width

--- src/pages/room-details/room-details.js
+++ src/pages/room-details/room-details.js
@@ -6,14 +6,14 @@ if (UIwrap) {
 }
 
 function drawGraph(great = 0, good = 0, avg = 0, poor = 0) {
-  if(UIgraphCanvas.getContext) {
+  if (UIgraphCanvas.getContext) {
     // define canvas rendering context
     const ctx = UIgraphCanvas.getContext('2d');
 
     const p2 = Math.PI * 2;
 
     // gap between arcs
-    const gap = p2 / 360 * 2,
+    const gap = (p2 / 360) * 2,
       hgap = gap / 2;
 
     // total votes count
@@ -22,45 +22,45 @@ function drawGraph(great = 0, good = 0, avg = 0, poor = 0) {
     // start draw from top
     let startAngle = 0 - p2 / 4 - hgap,
       endAngle;
-      
+
     // great reviews arc
     if (great) {
       // current arc length in radian
-      const greatArc = great / total * p2;
+      const greatArc = (great / total) * p2;
       endAngle = startAngle - greatArc + gap;
 
       // get top and bottom points of an arc
       let h1 = gradHeight(startAngle, endAngle).top;
       let h2 = gradHeight(startAngle, endAngle).bot;
-      
+
       // set gradient, color stops
       const greatColor = ctx.createLinearGradient(0, 0, 0, 120);
       greatColor.addColorStop(h1, '#ffe39c');
       greatColor.addColorStop(h2, '#ffba9c');
 
       // draw an arc
-      drawArc(ctx, startAngle, endAngle, greatColor, false);
+      drawArc(ctx, startAngle, endAngle, greatColor);
     }
-    
+
     // good reviews arc
     if (good) {
-      const goodArc = good / total * p2;
+      const goodArc = (good / total) * p2;
       startAngle = endAngle - gap;
       endAngle = startAngle - goodArc + gap;
 
       let h1 = gradHeight(startAngle, endAngle).top;
       let h2 = gradHeight(startAngle, endAngle).bot;
-      
-      const goodColor1 = ctx.createLinearGradient(0, 0, 0, 120);
-      goodColor1.addColorStop(h1, '#6fcf97');
-      goodColor1.addColorStop(h2, '#66d2ea');
-      
-      drawArc(ctx, startAngle, endAngle, goodColor1, false);
+
+      const goodColor = ctx.createLinearGradient(0, 0, 0, 120);
+      goodColor.addColorStop(h1, '#6fcf97');
+      goodColor.addColorStop(h2, '#66d2ea');
+
+      drawArc(ctx, startAngle, endAngle, goodColor);
     }
-    
+
     // average reviews arc
     if (avg) {
-      const avgArc = avg / total * p2;
+      const avgArc = (avg / total) * p2;
       startAngle = endAngle - gap;
       endAngle = startAngle - avgArc + gap;
 
@@ -71,12 +71,12 @@ function drawGraph(great = 0, good = 0, avg = 0, poor = 0) {
       avgColor.addColorStop(h1, '#bc9cff');
       avgColor.addColorStop(h2, '#8ba4f9');
 
-      drawArc(ctx, startAngle, endAngle, avgColor, true);
+      drawArc(ctx, startAngle, endAngle, avgColor);
     }
 
     // poor reviews arc
     if (poor) {
-      const poorArc = poor / total * p2;
+      const poorArc = (poor / total) * p2;
       startAngle = endAngle - gap;
       endAngle = startAngle - poorArc + gap;
 
@@ -87,7 +87,7 @@ function drawGraph(great = 0, good = 0, avg = 0, poor = 0) {
       poorColor.addColorStop(h1, '#909090');
       poorColor.addColorStop(h2, '#3d4975');
 
-      drawArc(ctx, startAngle, endAngle, poorColor, false);
+      drawArc(ctx, startAngle, endAngle, poorColor);
     }
 
     // display total vote counter
@@ -98,9 +98,9 @@ function drawGraph(great = 0, good = 0, avg = 0, poor = 0) {
   }
 }
 
-function drawArc(ctx, start, end, color, bold) {
-  const strokeWidth = bold ? 10 : 4,
-    radius = bold ? 63 - strokeWidth : 60 - strokeWidth;
+function drawArc(ctx, start, end, color) {
+  const strokeWidth = 4,
+    radius = 60 - strokeWidth;
 
   ctx.beginPath();
   ctx.arc(60, 60, radius, start, end, true);
@@ -113,8 +113,8 @@ function drawArc(ctx, start, end, color, bold) {
 function gradHeight(start, end) {
   let startHeight = (Math.sin(start) + 1) / 2,
     endHeight = (Math.sin(end) + 1) / 2,
-    topGradient = (startHeight > endHeight) ? endHeight : startHeight,
-    botGradient = (startHeight > endHeight) ? startHeight : endHeight;
+    topGradient = startHeight > endHeight ? endHeight : startHeight,
+    botGradient = startHeight > endHeight ? startHeight : endHeight;
 
   // check if arc crosses y axis, if it does - set bottomGradient = 1
   let axis = -1.5 * Math.PI;
