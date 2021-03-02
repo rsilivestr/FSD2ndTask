c40516b3411202ceb9d06243f8bf17424ab7875d
Roman Silivestrov <rsilivestr@gmail.com>
2021-03-02 18:44:56 +0300

    Write header as a class

--- src/components/header/FSD2TaskHeader.js
+++ src/components/header/FSD2TaskHeader.js
@@ -1,17 +1,4 @@
-const CLASS_NAMES = {
-  header: 'header',
-  nav: 'header__nav',
-  navOpen: 'header__nav--expanded',
-  btn: {
-    account: 'header__btn--type_account',
-    login: 'header__btn--type_login',
-    menu: 'header__btn--type_menu',
-    register: 'header__btn--type_register',
-    withBorder: 'button--style_bordered',
-    withGradient: 'button--style_gradient',
-    withIcon: 'header__btn--type_iconic',
-  },
-};
+import { CLASSES } from './constants';
 
 class FSD2ndTaskHeader {
   constructor(user = null) {
@@ -21,57 +8,94 @@ class FSD2ndTaskHeader {
   }
 
   _init() {
+    // Initialize UI object
+    this.UI = {};
+
     // Set ui elements
-    this.UI.header = document.querySelector(`.${CLASS_NAMES.header}`);
-    this.UI.nav = this.UI.header.querySelector(`.${CLASS_NAMES.nav}`);
-    this.UI.loginBtn = document.querySelector(`.${CLASS_NAMES.btn.login}`);
-    this.UI.regBtn = document.querySelector(`.${CLASS_NAMES.btn.reg}`);
-    this.UI.accountBtn = document.querySelector(`.${CLASS_NAMES.btn.account}`);
+    this.UI.header = document.querySelector(`.${CLASSES.header}`);
+    this.UI.nav = this.UI.header.querySelector(`.${CLASSES.nav}`);
+    this.UI.loginBtn = document.querySelector(`.${CLASSES.btn.login}`);
+    this.UI.menuBtn = document.querySelector(`.${CLASSES.btn.menu}`);
+    this.UI.registerBtn = document.querySelector(`.${CLASSES.btn.register}`);
+    this.UI.accountBtn = document.querySelector(`.${CLASSES.btn.account}`);
 
     // Set current layout depending viewport width
     this.isDesktop = window.innerWidth > 920 ? true : false;
+
+    // Toggle initial buttons variant
+    if (this.isDesktop) {
+      this._toggleDesktopButtons();
+    } else {
+      this._toggleMobileButtons();
+    }
 
     // Add event listeners
     this._addListeners();
   }
 
   _setButtonIcon(btn) {
-    btn.classList.classList.add(CLASS_NAMES.btn.withIcon);
-    btn.textContent = `<i class="material-icons">${btn.dataset.icon}</i>`;
+    btn.classList.add(CLASSES.btn.withIcon);
+    btn.innerHTML = `<i class="material-icons">${btn.dataset.icon}</i>`;
   }
 
   _setButtonText(btn) {
-    btn.classList.classList.remove(CLASS_NAMES.btn.withIcon);
-    btn.textContent = btn.dataset.name;
+    btn.classList.remove(CLASSES.btn.withIcon);
+    btn.innerHTML = btn.dataset.name;
+  }
+
+  _toggleDesktopButtons() {
+    if (this.user) {
+      // Toggle account btn icon
+      this._setButtonText(this.UI.accountBtn);
+    } else {
+      // Toggle login btn icon
+      this._setButtonText(this.UI.loginBtn);
+
+      // Toggle register btn icon
+      this._setButtonText(this.UI.registerBtn);
+    }
+  }
+
+  _toggleMobileButtons() {
+    if (this.user) {
+      // Toggle account button icon
+      this._setButtonIcon(this.UI.accountBtn);
+    } else {
+      // Toggle login button icon
+      this._setButtonIcon(this.UI.loginBtn);
+
+      // Toggle register button icon
+      this._setButtonIcon(this.UI.registerBtn);
+    }
   }
 
   _resizeHeader() {
     if (window.innerWidth < 920 && this.isDesktop) {
       // Toggle mobile layout
       this.isDesktop = false;
-
-      if (!this.user) {
-        // Toggle login btn icon
-        this._setButtonIcon(this.UI.loginBtn);
-
-        // Toggle register btn icon
-        this._setButtonIcon(this.UI.regBtn);
-      } else {
-        // Toggle account btn icon
-        this._setButtonIcon(this.UI.accountBtn);
-      }
+      this._toggleMobileButtons();
     } else if (window.innerWidth >= 920 && !this.isDesktop) {
       // Toggle desktop layout
       this.isDesktop = true;
+      this._toggleDesktopButtons();
     }
+
+    if (window.innerWidth >= 768) {
+      // Toggle mobile menu off
+      this.UI.nav.classList.remove(CLASSES.navOpen);
+    }
+  }
+
+  _toggleNav() {
+    this.UI.nav.classList.toggle(CLASSES.navOpen);
   }
 
   _addListeners() {
     // Update layout on resize
-    window.addEventListener('resize', this._resizeHeader);
+    window.addEventListener('resize', () => this._resizeHeader());
 
     // Toggle menu on click
-    this.UI.header.addEventListener('click', this._toggleNav);
+    this.UI.menuBtn.addEventListener('click', () => this._toggleNav());
   }
 }
 
---
+++ src/components/header/constants.js
@@ -0,0 +1,16 @@
+const CLASSES = {
+  header: 'header',
+  nav: 'header__nav',
+  navOpen: 'header__nav--expanded',
+  btn: {
+    account: 'header__btn--type_account',
+    login: 'header__btn--type_login',
+    menu: 'header__btn--type_menu',
+    register: 'header__btn--type_register',
+    withBorder: 'button--style_bordered',
+    withGradient: 'button--style_gradient',
+    withIcon: 'header__btn--type_iconic',
+  },
+};
+
+export { CLASSES };
